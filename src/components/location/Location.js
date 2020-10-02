b805978bc050bed3c99e3c2f31caff75107ed266
Jennifer Starkey <jenniferrstarkey@yahoo.com>
2020-10-02 13:07:39 -0500

    first commit

---
+++ src/components/location/Location.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Location.css"
+
+
+export const LocationCard = () => (
+    <section className="location">
+        <h3 className="location_name">Nashville Kennels North</h3>
+        <div className="lcoation_address">200 Main Street</div>
+    </section>
+)
